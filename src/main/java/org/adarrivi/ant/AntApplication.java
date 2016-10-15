75ca8d8c34da7a52c4f5ebf3ffe686d2077db29f
Alvaro de Miguel <adarrivi@gmail.com>
2016-10-15 23:02:41 +0200

    Skeleton of the project

---
+++ src/main/java/org/adarrivi/ant/AntApplication.java
@@ -0,0 +1,13 @@
+package org.adarrivi.ant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AntApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(AntApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Started");
+    }
+}
---
+++ src/test/java/org/adarrivi/ant/AntApplicationTest.java
@@ -0,0 +1,11 @@
+package org.adarrivi.ant;
+
+import org.testng.annotations.Test;
+
+public class AntApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
